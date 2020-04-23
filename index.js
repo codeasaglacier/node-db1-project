62a02d69ff208427ae3361acfe0765ccea5e3db4
codeasaglacier <remingtonshock@gmail.com>
2020-04-23 15:15:40 -0800

    wrote sql queries and CRUD endpoints, mvp

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const express = require( "express" )
 const server = require("./api/server.js");
+const accountsRouter = require( "./accountsRouter" )
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 4000;
+
+server.use( express.json() )
+server.use( "/accounts", accountsRouter )
+
+server.use( ( err, req, res, next ) => {
+  console.log( err )
+  res.status( 500 ).json( {
+    message: "Something went wrong"
+  } ) 
+} )
+
 
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
